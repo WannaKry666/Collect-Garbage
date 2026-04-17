ac38b7536c5b20e68fd00e7a98319411116eeec5
Dang Khoi <nghuynhdangkhoixyz@gmail.com>
2026-04-17 12:42:11 +0700

    feat: test deployment

--- backend/GarbageCollection.DataAccess/Interfaces/IWasteReportRepository.cs
+++ backend/GarbageCollection.DataAccess/Interfaces/IWasteReportRepository.cs
@@ -8,6 +8,7 @@ namespace GarbageCollection.DataAccess.Interfaces
         Task<WasteReport> CreateAsync(WasteReport report);
         Task<WasteReport?> GetByIdAsync(int id);
         Task<IEnumerable<WasteReport>> GetByCitizenIdAsync(int citizenId, ReportStatus? status = null);
+        Task<(IEnumerable<WasteReport> Items, int Total)> GetByCitizenIdPagedAsync(int citizenId, int page, int limit);
         Task<WasteReport> UpdateAsync(WasteReport report);
     }
 }
