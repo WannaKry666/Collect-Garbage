b252a4befdffaf354601ec79c23853d4def25f1f
Blue1316 <leanninh1316@gmail.com>
2026-04-18 20:58:53 +0700

    Delete backend directory

--- backend/GarbageCollection.Business/Interfaces/IAuthService.cs
+++
@@ -1,9 +0,0 @@
-using GarbageCollection.Common.DTOs;
-
-namespace GarbageCollection.Business.Interfaces
-{
-    public interface IAuthService
-    {
-        Task<string> RegisterAsync(RegisterDto dto);
-    }
-}
--- backend/GarbageCollection.Common/DTOs/Register/RegisterDto.cs
+++
@@ -1,6 +0,0 @@
-public class RegisterDto
-{
-    public string FullName { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
-}
